fbcadb8dcf3f3c37df008539dde88fd00c658818
Shailja Goswami <shailjagoswami5@gmail.com>
2025-07-08 07:19:46 +0000

    practice

---
+++ react-basics/components/props/src/App.jsx
@@ -0,0 +1,14 @@
+import UserCard from "./components.jsx/UserCard"
+
+function App() {
+
+  return (
+    <div>
+    <UserCard />
+    <UserCard />
+    <UserCard />
+    </div>
+  )
+}
+
+export default App
---
+++ react-basics/components/props/src/components.jsx/UserCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const UserCard = () => {
+  return (
+    <div className='user-container'>
+        <p id='user-name'>shailja</p>
+        <img id='user-img' src='' alt='flower'></img>
+        <p id='user-description'>bloom like a flower</p>
+    </div>
+  )
+}
+
+export default UserCard
